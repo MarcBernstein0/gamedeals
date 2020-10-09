b8dd7e0359b4f5cb8731cf883d779060149e68fa
Marc Bernstein <marcbernstein98@gmail.com>
2020-10-09 16:00:45 -0400

    Starting to build main purpose of application

---
+++ api/buisnesslogic/finddeals.go
@@ -0,0 +1,7 @@
+package buisnesslogic
+
+import "github.com/MarcBernstein0/gamedeals/api/model"
+
+func findGameDeals(request model.Request) (model.Response, error) {
+	return model.Response{}, nil
+}
